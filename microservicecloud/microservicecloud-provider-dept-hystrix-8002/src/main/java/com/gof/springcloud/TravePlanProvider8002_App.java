d464451b82c2989f638a3134764be86808488d7f
timothy <timhou@outlook.com>
2020-10-26 14:04:52 +0800

    remove eureka

--- microservicecloud/microservicecloud-provider-dept-hystrix-8002/src/main/java/com/gof/springcloud/TravePlanProvider8002_App.java
+++ microservicecloud/microservicecloud-provider-dept-hystrix-8002/src/main/java/com/gof/springcloud/TravePlanProvider8002_App.java
@@ -4,12 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient //Register into eureka after initiation
-@EnableDiscoveryClient
+//@EnableEurekaClient //Register into eureka after initiation
+//@EnableDiscoveryClient
 @EnableCircuitBreaker //import hystrix for fusing mechanism
 @EnableCaching
 public class TravePlanProvider8002_App
